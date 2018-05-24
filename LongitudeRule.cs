6adca85b89e72d6b09c0e7330e7bd52c9a3543ff
sidneyjohn23 <sidneyjohn23@icloud.com>
2018-05-24 16:48:02 +0200

    Initial commit

---
+++ LongitudeRule.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PhysikLaborSatellit
+{
+	public class LongitudeRule : ValidationRule
+	{
+		public double Min => 0;
+		public double Max => 180;
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			double longitude = 0;
+			try
+			{
+				if (((string)value).Length > 0)
+				{
+					longitude = double.Parse((string)value, NumberStyles.Float);
+				}
+			}
+			catch (Exception)
+			{
+				return new ValidationResult(false, "Keine Zahl eingegeben");
+			}
+			if (longitude < Min || longitude > Max)
+			{
+				return new ValidationResult(false, "Außerhalb des zulässigen Bereichs");
+			}
+			return ValidationResult.ValidResult;
+		}
+	}
+
+	public class LatitudeRule : ValidationRule
+	{
+		public double Min => 0;
+		public double Max => 90;
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			double latitude = 0;
+			try
+			{
+				if (((string)value).Length > 0)
+				{
+					latitude = double.Parse((string)value);
+				}
+			}
+			catch (Exception)
+			{
+				return new ValidationResult(false, "Keine Zahl eingegeben");
+			}
+			if (latitude < Min || latitude > Max)
+			{
+				return new ValidationResult(false, "Außerhalb des zulässigen Bereichs");
+			}
+			return ValidationResult.ValidResult;
+		}
+	}
+
+	public class MyDataSource
+	{
+		public double Longitude { get; set; }
+		public double Latitude { get; set; }
+		public double LongitudeSat { get; set; }
+
+		public MyDataSource() => Longitude = Latitude = LongitudeSat = 0;
+	}
+
+	public class DoubleToPersistantStringConverter : IValueConverter
+	{
+		private string lastConvertBackString;
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is double)) return null;
+
+			string stringValue = lastConvertBackString ?? value.ToString();
+			lastConvertBackString = null;
+
+			return stringValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is string)) return null;
+
+			if (double.TryParse((string)value, out double result))
+			{
+				lastConvertBackString = (string)value;
+				return result;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ SatelliteAntennaCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysikLaborSatellit
+{
+	internal static class SatelliteAntennaCalculator
+	{
+		const double k = 0.151;
+		/// <summary>
+		/// Gibt den Azimutwinkel zurück
+		/// </summary>
+		/// <param name="lambda">Längengraddifferenz im Bogenmaß</param>
+		/// <param name="beta">Breitengrad im Bogenmaß</param>
+		/// <returns>Azimutwinkel im Bogenmaß</returns>
+		internal static double GetAzimutAngle(double lambda, double beta) => Math.Atan(Math.Tan(lambda) / Math.Sin(beta)) + ((beta > 0) ? 180 : 0);
+
+		/// <summary>
+		/// Gibt den Elevationswinkel zurück
+		/// </summary>
+		/// <param name="lambda">Längengraddifferenz im Bogenmaß</param>
+		/// <param name="beta">Breitengrad im Bogenmaß</param>
+		/// <returns>Elevationswinkel im Bogengrad</returns>
+		internal static double GetElevationAngle(double lambda, double beta)
+		{
+			double alpha = (Math.Cos(lambda) * Math.Cos(beta) - k) / (Math.Sqrt(1 - Math.Pow(Math.Cos(lambda), 2) * Math.Pow(Math.Cos(beta), 2)));
+			if ((lambda > 0 && beta < 0) || (lambda < 0 && beta > 0))
+				alpha *= -1;
+			return alpha;
+		}
+
+		/// <summary>
+		/// Gibt den Elevationswinkel abhängig von Azimutwinkel und Breitengrad aus.
+		/// </summary>
+		/// <param name="psi">Azimutwinkel im Bogenmaß</param>
+		/// <param name="beta">Breitengrad im Bogenmaß</param>
+		/// <returns>Elevationswinkel im Bogenmaß</returns>
+		internal static double GetElevationCurve(double psi, double beta)
+		{
+			double test = Math.Cos(psi) / Math.Tan(beta);
+			double ergebnis = test - k * (Math.Sqrt(Math.Pow(test, 2) + 1));
+			if (beta < 0)
+				ergebnis *= -1;
+			return ergebnis;
+		}
+
+
+	}
+}
